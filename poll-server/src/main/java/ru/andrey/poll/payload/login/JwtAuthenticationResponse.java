1c396f4c3ad1cf4bb97e2de7ec17fa48657b0e59
AntipovAndrey <antipov11.11@gmail.com>
2018-10-11 11:06:07 +0300

    login/signup request and response models

---
+++ poll-server/src/main/java/ru/andrey/poll/payload/login/JwtAuthenticationResponse.java
@@ -0,0 +1,16 @@
+package ru.andrey.poll.payload.login;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JwtAuthenticationResponse {
+
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public JwtAuthenticationResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
---
+++ poll-server/src/main/java/ru/andrey/poll/payload/login/LoginRequest.java
@@ -0,0 +1,17 @@
+package ru.andrey.poll.payload.login;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+    @NotBlank
+    private String usernameOrEmail;
+
+    @NotBlank
+    private String password;
+}
