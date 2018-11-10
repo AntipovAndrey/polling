d30c1b259a4fd5779accf58df7d45b612036bbb4
AntipovAndrey <antipov11.11@gmail.com>
2018-11-10 20:11:12 +0300

    remove unused services

--- poll-server/src/main/java/ru/andrey/poll/service/VoteService.java
+++
@@ -1,7 +0,0 @@
-package ru.andrey.poll.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface VoteService {
-}
--- poll-server/src/main/java/ru/andrey/poll/service/VoteServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package ru.andrey.poll.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class VoteServiceImpl implements VoteService {
-}
